144ab9f0b5f39b30eda58c3777866742c0bca2ba
lindamiller <lindamiller47@protonmail.com>
2020-08-19 11:16:59 -0600

    Update from branch

--- helloWorld.c
+++ helloWorld.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, World! from Linda with write access");
+   printf("Hello, World! from Linda with write access on her branch");
    return 0;
 }
