d9e4b91cbacb6dd8f6cf460844b7794db2589188
lindamiller <lindamiller47@protonmail.com>
2020-08-19 11:15:53 -0600

    Update

--- helloWorld.c
+++ helloWorld.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, World! from Linda");
+   printf("Hello, World! from Linda with write access");
    return 0;
 }
